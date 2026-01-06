794129d02d0fdc5627c94430f9f66c28e4f86eaf
Apple <apple@Apples-MacBook-Pro.local>
2026-01-06 12:58:37 +0530

    2Sum Optimized code

--- Easy/2Sum/code.py
+++ Easy/2Sum/code.py
@@ -4,11 +4,12 @@ class Solution:
             return []
         left, right = 0, len(nums)-1
         map= dict()
-        for cur_ind in range(0,len(nums)):
-            map[nums[cur_ind]]= cur_ind
         for i in range(0, len(nums)):
-            first_ele = nums[i]
+            curr_ele = nums[i]
             second_ele = target-nums[i]
             if second_ele in map and map[second_ele] !=i:
                 return [i, map[second_ele]]
+            else:
+                # Add to map for further checks
+                map[curr_ele] = i
         return []
