cbfc6fe87554b4e335d45f290eb7bf6a5da26bf5
Apple <apple@Apples-MacBook-Pro.local>
2026-01-17 15:20:35 +0530

    Added one more optimal solution for valid anagrams

--- Strings/Easy/Valid-Anagram/code.py
+++ Strings/Easy/Valid-Anagram/code.py
@@ -56,6 +56,9 @@ class BrutForce2:
     Time: O(n*n)
     Space: O(n) Storing indexes in set
 
+    Approach:
+        comparing index sets lengths of two strings
+
     """
     def isAnagram(self, s, t):
         if len(s) != len(t):
@@ -70,84 +73,58 @@ class BrutForce2:
                     break
 
         return len(index_set) == len(s)
-       
+    
+class OptimalSolution:
+    """
+    Time: O(n)
+    Space: O(n) Storing each char frequency
 
+    Approach: 
+        Frequency increment and decrement
+
+    """
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = {}
+        # Storing each char frequency
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+        # Checking second string chars with frequency
+        for char in t:
+            if char not in count or count[char] == 0:
+                return False
+            count[char] -= 1
+
+        return True
+#If inputs strings are only small case alphabets
 
 class OptimalSolution:
     """
-    Time: O(n*m) 
-    Space: O(m)
-        Where m is the unique chars possible to pass as input string
+    Time: O(n)
+    Space: O(n) Storing each char frequency
+
+    Approach: 
+        Frequency increment and decrement
+
+    """
+    def isAnagram(a, b):
+        if len(a) != len(b):
+            return False
+        # 26 containers for 26 alphabets and storing their frequencies
+        freq = [0]*26
+        # Character frequency increment
+        for c in a:
+            freq[ord(c)-97] += 1
+        # Character frequence decrement
+        for c in b:
+            freq[ord(c)-97] -= 1
+        
+        # if we came to initial state where all char frequencies become 0, then two strings anagrams
+        return all(x == 0 for x in freq)
+
+
+
     
-    Time complexity:
-        n for visiting each char * m for time taking to fetch most frequent chars out of unique chars in input array
-        If we wil get only same case alphabets, then unique or distinct chars will be A-A ie 26, then
-        O(n*26) ~ O(n) where 26 is consider as constant
-
-    Space complexity:
-        We are storing the each unique char frequency.If we pass only upper or lowercase alphabets then it will become O(26) and it will considered as constant
-        then O(1) space
-    """
-    def characterReplacement(self, s, k):
-        left, right = 0, 0
-        curr_window_char_frequency = dict()
-        max_length = 0
-        while right<len(s):
-            if s[right] not in curr_window_char_frequency:
-                curr_window_char_frequency[s[right]]=1
-            else:
-                curr_window_char_frequency[s[right]]+=1
-            
-            # Getting most occured char value of the current window
-            most_frequent_value = 0
-            for key, value in curr_window_char_frequency.items():
-                most_frequent_value = max(most_frequent_value, value)
-
-            no_of_chars_to_replace = (right+1-left)-most_frequent_value # (right+1-left) gives current window length
-            valid_window = no_of_chars_to_replace<= k
-
-            # More chars to replace
-            if not valid_window:
-                # Reducing left pointer char frequency
-                curr_window_char_frequency[s[left]]-=1
-                left+=1
-
-            curr_window_lenght = right+1-left
-
-            # Updating max size window length
-            max_length = max(max_length,curr_window_lenght)
-
-            right+=1
-        return max_length
-
-
-# Line of Code optimization of above one
-# Reduced O(26) extra time for getting max frequency
-class OptimalSolution1:
-    """
-    Time: O(n) 
-    Space: O(m) => O(26) => O(1)
-    """
-    def characterReplacement(self, s, k) -> int:
-        left = 0
-        freq = {}
-        max_freq = 0
-        max_len = 0
-
-        for right in range(len(s)):
-            freq[s[right]] = freq.get(s[right], 0) + 1
-            max_freq = max(max_freq, freq[s[right]])
-
-            """
-            Checking the window is valid or not if not valid
-            Removing the left pointer frequency
-            Moving left pointer to rightward direction
-            """
-            while (right - left + 1) - max_freq > k:
-                freq[s[left]] -= 1
-                left += 1
-
-            max_len = max(max_len, right - left + 1)
-
-        return max_len
 
